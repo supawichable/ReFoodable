8e2847ad90a676e2e9af3f994c3144914d54ca5b
Quatton <atomicatives@gmail.com>
2023-02-13 23:12:43 +0900

    Added test for restaurant

--- test/json_parsing_test.dart
+++ test/json_parsing_test.dart
@@ -1,20 +1,39 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:gdsctokyo/models/user/_user.dart';
+import 'dart:math';
 
-// will later on use Firebase Emulators
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gdsctokyo/models/restaurant/_restaurant.dart';
+
 void main() {
-  group('User', () {
-    test('Parsing not authenticated user', () {
-      const json = {
-        "id": "12345",
-        "authenticated": false,
-        "email_verified": false
+  group('restaurant', () {
+    test('Parse restaurant', () async {
+      final json = {
+        "id": "test",
+        "name": "Restaurant 1",
+        "location": {
+          "latitude": 50.0,
+          "longitude": 128.0,
+        },
+        "updated_at": "2021-08-01T00:00:00.000Z",
+        "created_at": "2021-08-01T00:00:00.000Z",
+        "address": "test",
+        "email": "test@example.com",
+        "phone": "08080808080",
+        "owner_id": "test",
+        "category": ["japanese"],
       };
 
-      final user = User.fromJson(json);
-      expect(user.id, "12345");
-      expect(user.authenticated, false);
-      expect(user.emailVerified, false);
+      final restaurant = Restaurant.fromJson(json);
+      expect(restaurant.id, "test");
+      expect(restaurant.name, "Restaurant 1");
+      expect(restaurant.location.latitude, 50.0);
+      expect(restaurant.location.longitude, 128.0);
+      expect(restaurant.updatedAt, DateTime.parse("2021-08-01T00:00:00.000Z"));
+      expect(restaurant.createdAt, DateTime.parse("2021-08-01T00:00:00.000Z"));
+      expect(restaurant.address, "test");
+      expect(restaurant.email, "test@example.com");
+      expect(restaurant.phone, "08080808080");
+      expect(restaurant.ownerId, "test");
+      expect(restaurant.category, [FoodCategory.japanese]);
     });
   });
 }
