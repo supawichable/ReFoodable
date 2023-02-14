0b51d6c8df8ca7092e12da9ab44dbf9fd5b2bc68
Quatton <atomicatives@gmail.com>
2023-02-14 13:45:41 +0900

    Finalized Firebase Set-up

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:gdsctokyo/firebase_options.dart';
 import 'package:gdsctokyo/routes/router.gr.dart';
 import 'package:gdsctokyo/theme/color_schemes.g.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  if (kDebugMode && dotenv.get("USE_EMULATOR") == 'true') {
+    try {
+      FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+      await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+    } catch (e) {
+      print(e);
+    }
+  }
+
   runApp(MyApp());
 }
 
