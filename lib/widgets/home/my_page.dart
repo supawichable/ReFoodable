aa55bd0382e4f5aa308cd7808bb4e76f3adcd954
Quatton <atomicatives@gmail.com>
2023-02-21 10:18:08 +0700

    Wrap entire profile card with StreamBuilder

--- lib/widgets/home/my_page.dart
+++ lib/widgets/home/my_page.dart
@@ -27,57 +27,59 @@ class ProfileCard extends StatelessWidget {
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                CircleAvatar(
-                  radius: 30,
-                  backgroundImage: NetworkImage(FirebaseAuth
-                          .instance.currentUser?.photoURL ??
-                      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'),
-                ),
-                const SizedBox(width: 16),
-                if (FirebaseAuth.instance.currentUser?.isAnonymous ?? true)
-                  Text('You\'re not signed in.',
-                      style: Theme.of(context).textTheme.bodyLarge)
-                else
-                  Text(
-                      FirebaseAuth.instance.currentUser?.displayName ?? 'Guest',
-                      style: Theme.of(context).textTheme.bodyLarge),
-              ],
-            ),
-            if (FirebaseAuth.instance.currentUser?.isAnonymous ?? true)
-              const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  softWrap: true,
-                  'Sign in to save your favorites and participate in the community!',
-                ),
-              ),
-            StreamBuilder<User?>(
-              stream: FirebaseAuth.instance.authStateChanges(),
-              builder: (BuildContext context, snapshot) {
-                final user = snapshot.data;
+        child: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (BuildContext context, snapshot) {
+            final user = snapshot.data;
+            final _isAnonymous =
+                FirebaseAuth.instance.currentUser?.isAnonymous ?? true;
 
-                if (user?.isAnonymous ?? true) {
-                  return ElevatedButton(
+            return Column(
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: NetworkImage(FirebaseAuth
+                              .instance.currentUser?.photoURL ??
+                          'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'),
+                    ),
+                    const SizedBox(width: 16),
+                    if (_isAnonymous)
+                      Text('You\'re not signed in.',
+                          style: Theme.of(context).textTheme.bodyLarge)
+                    else
+                      Text(
+                          FirebaseAuth.instance.currentUser?.displayName ??
+                              'Guest',
+                          style: Theme.of(context).textTheme.bodyLarge),
+                  ],
+                ),
+                if (_isAnonymous)
+                  const Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      softWrap: true,
+                      'Sign in to save your favorites and participate in the community!',
+                    ),
+                  ),
+                if (_isAnonymous)
+                  ElevatedButton(
                     onPressed: () {
                       context.router.push(const SignInRoute());
                     },
                     child: const Text('Sign In'),
-                  );
-                } else {
-                  return ElevatedButton(
+                  )
+                else
+                  ElevatedButton(
                     onPressed: () {
                       FirebaseAuth.instance.signOut();
                     },
                     child: const Text('Sign Out'),
-                  );
-                }
-              },
-            ),
-          ],
+                  )
+              ],
+            );
+          },
         ),
       ),
     );
