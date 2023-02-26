3af2d36f0df9d7345d608a061fb54e6590e9fff3
Quatton <atomicatives@gmail.com>
2023-02-26 17:23:15 +0700

    added UserPrivate and UserPublic

---
+++ lib/models/private/_private.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:gdsctokyo/extension/firebase_extension.dart';
+import 'package:gdsctokyo/util/json_converter.dart';
+
+part '_private.g.dart';
+part '_private.freezed.dart';
+
+part 'private.dart';
---
+++ lib/models/public/public.dart
@@ -0,0 +1,19 @@
+part of '_public.dart';
+
+@freezed
+class UserPublic with _$UserPublic {
+  const factory UserPublic({
+    String? displayName,
+    String? photoURL,
+  }) = _UserPublic;
+
+  factory UserPublic.fromJson(Map<String, dynamic> json) =>
+      _$UserPublicFromJson(json);
+
+  factory UserPublic.fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot) {
+    return UserPublic.fromJson(snapshot.data()!);
+  }
+
+  static Map<String, dynamic> toFirestore(UserPublic data) => data.toJson();
+}
