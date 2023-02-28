27edfe0de809c2a5b2af13bc3cb7c98f572ff801
Quatton <atomicatives@gmail.com>
2023-03-01 01:05:04 +0700

    Upload a cover photo successful

--- lib/screens/add_a_store.dart
+++ lib/screens/add_a_store.dart
@@ -1,6 +1,14 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_layout_grid/flutter_layout_grid.dart';
+import 'package:gdsctokyo/models/image_upload/_image_upload.dart';
 import 'package:gdsctokyo/models/store/_store.dart';
+import 'package:gdsctokyo/providers/image_upload.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:image_cropper/image_cropper.dart';
 
 class AddAStorePage extends StatefulWidget {
   const AddAStorePage({super.key});
@@ -30,7 +38,7 @@ class _AddAStorePageState extends State<AddAStorePage> {
   late final TextEditingController _addressController;
   late final TextEditingController _emailController;
   late final TextEditingController _phoneController;
-  String? _photoURL;
+  File? _coverPhoto;
   List<FoodCategory> _categoryList = [];
 
   @override
@@ -51,61 +59,102 @@ class _AddAStorePageState extends State<AddAStorePage> {
     super.dispose();
   }
 
+  void _setFile(File file) {
+    setState(() {
+      _coverPhoto = file;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add a Store'),
       ),
-      body: Column(
-        children: [
-          // a rectangular area to display the photo
-          // if photoURL is null, display a dotted border with
-          // "Upload a cover photo" text and an upload icon in the middle
-          // if photoURL is not null, display the photoURL
-          Container(
-            height: 200,
-            width: double.infinity,
-            color: Theme.of(context).colorScheme.primaryContainer,
-            child: _photoURL == null
-                ? const Center(
-                    child: Text('Upload a cover photo'),
-                  )
-                : Image.network(_photoURL!),
-          ),
-          // form
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  // name
-                  TextFormField(
-                    controller: _nameController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Store Name',
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a name';
-                      }
-                      return null;
-                    },
-                  ),
-                  // location
-                  ElevatedButton.icon(
-                      onPressed: () {
-                        _location = const GeoPoint(0, 0);
-                      },
-                      icon: const Icon(Icons.location_on),
-                      label: const Text('Set Location')),
-                ],
+      body: Form(
+          key: _formKey,
+          // child is a grid view of the fields
+          // fields will consist of a label and a text field
+          // except the cover photo on the top spanning the whole width
+          // the location will be a button that will set the coordinates to
+          // GeoPoint(0, 0) for now
+          // and the category field which will be a dropdown menu
+          child: LayoutGrid(
+            areas: '''
+            photo             photo
+            name_label        name_field
+            location_label    location_field
+            address_label     address_field
+            email_label       email_field
+            phone_label       phone_field
+            category_label    category_field
+            submit_button     submit_button
+          ''',
+            columnSizes: [200.px, auto],
+            rowSizes: [
+              300.px,
+              1.fr,
+              1.fr,
+              1.fr,
+              1.fr,
+              1.fr,
+              1.fr,
+              1.fr,
+            ],
+            children: [
+              CoverPhoto(
+                coverPhoto: _coverPhoto,
+                setFile: _setFile,
+              ).inGridArea('photo'),
+            ],
+          )),
+    );
+  }
+}
+
+// cover photo
+class CoverPhoto extends HookConsumerWidget {
+  final File? coverPhoto;
+  final void Function(File) setFile;
+  const CoverPhoto({
+    super.key,
+    required this.coverPhoto,
+    required this.setFile,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return DottedBorder(
+      borderType: BorderType.RRect,
+      child: SizedBox.expand(
+        child: Stack(
+          children: [
+            if (coverPhoto != null)
+              Image.file(
+                coverPhoto!,
+                fit: BoxFit.cover,
+              ),
+            Positioned.fill(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () async {
+                    final imageUpload = await ImageUploader(ref,
+                        options: ImageUploadOptions(
+                          aspectRatioPresets: [CropAspectRatioPreset.ratio3x2],
+                        )).handleImageUpload();
+                    imageUpload.whenOrNull(
+                        cropped: (file) => setFile(File(file.path)),
+                        error: (error) =>
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text(error.message),
+                            )));
+                  },
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
