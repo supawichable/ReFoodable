cff9e21e9d792e0ce679c7168a2b72f680104bfc
tonklable <tonklalor2544@gmail.com>
2023-03-15 11:30:48 +0900

    My Items Edited

--- lib/widgets/my_items.dart
+++ lib/widgets/my_items.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:gdsctokyo/extension/firebase_extension.dart';
 import 'package:gdsctokyo/models/item/_item.dart';
 import 'package:gdsctokyo/routes/router.gr.dart';
 import 'package:gdsctokyo/widgets/item_card.dart';
@@ -20,6 +22,12 @@ class MyItems extends StatefulWidget {
 }
 
 class _MyItemsState extends State<MyItems> {
+  late Stream<QuerySnapshot<Item>> top3Items = FirebaseFirestore.instance.stores
+      .doc(widget.storeId)
+      .myItems
+      .orderBy('updated_at', descending: true)
+      .limit(3)
+      .snapshots();
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,6 +45,57 @@ class _MyItemsState extends State<MyItems> {
         ),
         const SizedBox(
           height: 12,
+        ),
+        StreamBuilder(
+          stream: top3Items,
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot<Item>> snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: LinearProgressIndicator(),
+              );
+            }
+
+            return (snapshot.data == null || snapshot.data!.docs.isEmpty)
+                ? GestureDetector(
+                    onTap: () {
+                      context.router.push(StoreMyItemRoute(
+                        storeId: widget.storeId,
+                      ));
+                    },
+                    child: Container(
+                      height: 64,
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.surface,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Theme.of(context)
+                                  .shadowColor
+                                  .withOpacity(0.25),
+                              spreadRadius: 2,
+                              blurRadius: 4,
+                              offset: const Offset(0, 3),
+                            )
+                          ]),
+                      child: Center(
+                        child: Text("My Items Are Empty."),
+                      ),
+                    ),
+                  )
+                : Column(
+                    children: snapshot.data!.docs
+                        .map((DocumentSnapshot<Item> snapshot) {
+                      return ItemCard(
+                        key: ValueKey(snapshot.id),
+                        snapshot: snapshot,
+                      );
+                    }).toList(),
+                  );
+          },
         ),
         GestureDetector(
           onTap: () {
