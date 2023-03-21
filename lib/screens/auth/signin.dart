33bd5837e8029b3c560f759d3f553127564bf53f
Quatton <atomicatives@gmail.com>
2023-03-21 20:24:40 +0900

    signup with uploaded profileimg

--- lib/screens/auth/signin.dart
+++ lib/screens/auth/signin.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:gdsctokyo/providers/current_user.dart';
@@ -387,8 +388,16 @@ class _SignInPageState extends ConsumerState<SignInPage> {
         await FirebaseAuth.instance.currentUser!.sendEmailVerification();
         await FirebaseAuth.instance.currentUser!
             .updateDisplayName(_formKey.currentState!.fields['name']!.value);
-        await FirebaseAuth.instance.currentUser!
-            .updatePhotoURL('$placeholderUrlbase$seed');
+        if (_formKey.currentState!.fields['profileImage']!.value != null) {
+          final ref = FirebaseStorage.instance
+              .ref()
+              .child(FirebaseAuth.instance.currentUser!.uid)
+              .child('profile.jpg');
+          await ref
+              .putFile(_formKey.currentState!.fields['profileImage']!.value);
+          await FirebaseAuth.instance.currentUser!
+              .updatePhotoURL(await ref.getDownloadURL());
+        }
 
         if (mounted) {
           context.router.replace(const HomeRoute());
