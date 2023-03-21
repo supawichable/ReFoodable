8a84a7a3497dca122ae24b25c6ac8034aa65922a
Quatton <atomicatives@gmail.com>
2023-03-21 21:22:01 +0900

    store_form

--- lib/models/store/_store.dart
+++ lib/models/store/_store.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dart_geohash/dart_geohash.dart';
+import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:gdsctokyo/extension/geo_fire.dart';
 import 'package:gdsctokyo/util/json_converter.dart';
